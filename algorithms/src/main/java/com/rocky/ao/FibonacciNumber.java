7427f3e84e11b2d1bd7fcd14910f1a321e23aa3a
Yun Ao <yun.ao@ninebot.com>
2022-11-07 18:00:52 +0800

    add english annotations

--- algorithms/src/main/java/com/rocky/ao/FibonacciNumber.java
+++ algorithms/src/main/java/com/rocky/ao/FibonacciNumber.java
@@ -15,6 +15,13 @@ import com.rocky.utils.Times;
  *
  * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
  *
+ * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+ * such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+ *
+ * F(0) = 0, F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2), for n > 1.
+ * Given n, calculate F(n).
+ *
  */
 public class FibonacciNumber {
     public static int fib1(int number) {
