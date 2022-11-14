cb85af65999cd44e55460e29ce1a020ebe427831
Yun Ao <yun.ao@ninebot.com>
2022-11-14 16:01:43 +0800

    reverse linked list

---
+++ algorithms/src/main/java/com/rocky/ao/linkedlist/ReverseLinkedList206.java
@@ -0,0 +1,14 @@
+package com.rocky.ao.linkedlist;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/14 15:59
+ * @description
+ * https://leetcode.com/problems/reverse-linked-list/
+ * https://leetcode.cn/problems/reverse-linked-list/
+ */
+public class ReverseLinkedList206 {
+    public ListNode reverseListRecursion(ListNode head) {
+        return null;
+    }
+}
