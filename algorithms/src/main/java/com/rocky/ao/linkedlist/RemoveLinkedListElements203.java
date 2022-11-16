bef2d5d7b3c424bcec652320db39dca0fb9235c7
Yun Ao <yun.ao@ninebot.com>
2022-11-16 14:34:31 +0800

    remove linked list element recrusion

--- algorithms/src/main/java/com/rocky/ao/linkedlist/RemoveLinkedListElements203.java
+++ algorithms/src/main/java/com/rocky/ao/linkedlist/RemoveLinkedListElements203.java
@@ -4,6 +4,23 @@ package com.rocky.ao.linkedlist;
  * @author yun.ao
  * @date 2022/11/16 14:19
  * @description
+ * https://leetcode.com/problems/remove-linked-list-elements/
+ * https://leetcode.cn/problems/remove-linked-list-elements
  */
 public class RemoveLinkedListElements203 {
+    // recrusion
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        head.next = removeElements(head.next, val);
+
+        return head.val == val ? head.next : head;
+    }
+
+    public ListNode removeElements2(ListNode head, int val) {
+        // interation
+        
+        return null;
+    }
 }
