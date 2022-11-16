5c9fda8f7d1a87828414bd103eb72dc291d9616e
Yun Ao <yun.ao@ninebot.com>
2022-11-16 16:29:05 +0800

    add middle node class

---
+++ algorithms/src/main/java/com/rocky/ao/linkedlist/MiddleNode876.java
@@ -0,0 +1,11 @@
+package com.rocky.ao.linkedlist;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/16 16:28
+ * @description
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ * https://leetcode.cn/problems/middle-of-the-linked-list/
+ */
+public class MiddleNode876 {
+}
