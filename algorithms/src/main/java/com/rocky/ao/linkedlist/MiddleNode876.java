3d1a3281e00d177e39d4746ce22a6b92e9f9e046
Yun Ao <yun.ao@ninebot.com>
2022-11-16 16:47:25 +0800

    middle node 876

--- algorithms/src/main/java/com/rocky/ao/linkedlist/MiddleNode876.java
+++ algorithms/src/main/java/com/rocky/ao/linkedlist/MiddleNode876.java
@@ -8,4 +8,24 @@ package com.rocky.ao.linkedlist;
  * https://leetcode.cn/problems/middle-of-the-linked-list/
  */
 public class MiddleNode876 {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // fast and slow pointer
+        // start position both head
+        // when fast pointer wents to end, the slow pointer will in middle.
+        // time complexity O(N)
+        // space complexity O(1)
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+        return slowPointer;
+    }
 }
