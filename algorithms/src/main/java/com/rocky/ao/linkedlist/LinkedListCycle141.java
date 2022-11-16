f9bb8afb82531bd90bf59850dcde1045efa8a3f6
Yun Ao <yun.ao@ninebot.com>
2022-11-16 10:50:49 +0800

    add linked list cycle class

---
+++ algorithms/src/main/java/com/rocky/ao/linkedlist/LinkedListCycle141.java
@@ -0,0 +1,11 @@
+package com.rocky.ao.linkedlist;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/16 10:50
+ * @description
+ * https://leetcode.cn/problems/linked-list-cycle/
+ * https://leetcode.com/problems/linked-list-cycle/
+ */
+public class LinkedListCycle141 {
+}
