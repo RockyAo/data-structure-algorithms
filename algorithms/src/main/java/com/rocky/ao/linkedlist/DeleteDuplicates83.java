6db3cceaf8e56db607a06be65781950f6cf911fd
Yun Ao <yun.ao@ninebot.com>
2022-11-16 16:27:39 +0800

    delete duplicates

--- algorithms/src/main/java/com/rocky/ao/linkedlist/DeleteDuplicates83.java
+++ algorithms/src/main/java/com/rocky/ao/linkedlist/DeleteDuplicates83.java
@@ -6,4 +6,21 @@ package com.rocky.ao.linkedlist;
  * @description
  */
 public class DeleteDuplicates83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
 }
