0bdf00473ec8a3e4a4fef8c9bfb86bd984a2b62b
Yun Ao <yun.ao@ninebot.com>
2022-11-18 10:16:50 +0800

    add a test class use to check Java standard library code

---
+++ data-structures/src/main/java/com/rocky/ao/linkedlist/StandardLibraryTests.java
@@ -0,0 +1,14 @@
+package com.rocky.ao.linkedlist;
+
+import java.util.LinkedList;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/17 17:32
+ * @description
+ */
+public class StandardLibraryTests {
+    public static void main(String[] args) {
+        new LinkedList<int>();
+    }
+}
