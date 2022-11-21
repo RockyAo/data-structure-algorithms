039c1389862f0b6c33008ae5b36ed4eaf5d76819
Yun Ao <yun.ao@ninebot.com>
2022-11-21 17:56:43 +0800

    evaluate reverse polish notation

--- algorithms/src/main/java/com/rocky/ao/stack/EvaluateReversePolishNotation150.java
+++ algorithms/src/main/java/com/rocky/ao/stack/EvaluateReversePolishNotation150.java
@@ -1,9 +1,45 @@
 package com.rocky.ao.stack;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * @author yun.ao
  * @date 2022/11/21 17:47
  * @description
  */
 public class EvaluateReversePolishNotation150 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String element: tokens) {
+            if (isNumber(element)) {
+                stack.push(Integer.parseInt(element));
+            } else {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+
+                switch (element) {
+                    case "+":
+                        stack.push(num1 + num2);
+                        break;
+                    case "-":
+                        stack.push(num1 - num2);
+                        break;
+                    case "*":
+                        stack.push(num1 * num2);
+                        break;
+                    case "/":
+                        stack.push(num1 / num2);
+                        break;
+                    default:
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public boolean isNumber(String token) {
+        return !("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token));
+    }
 }
