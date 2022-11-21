08d43a745cb2337a7496bbca35ddc9fd1d64512d
Yun Ao <yun.ao@ninebot.com>
2022-11-21 20:03:06 +0800

    queue implements

--- data-structures/src/main/java/com/rocky/ao/queue/Queue.java
+++ data-structures/src/main/java/com/rocky/ao/queue/Queue.java
@@ -1,9 +1,36 @@
 package com.rocky.ao.queue;
 
+import java.util.LinkedList;
+
 /**
  * @author yun.ao
  * @date 2022/11/21 19:36
  * @description
  */
-public class Queue {
+public class Queue<E> {
+    private LinkedList<E> elements = new LinkedList<>();
+
+    public int size() {
+        return elements.size();
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    public void enqueue(E element) {
+        elements.add(element);
+    }
+
+    public E dequeue() {
+        return elements.removeFirst();
+    }
+
+    public E peek() {
+        return elements.getFirst();
+    }
+
+    public void clear() {
+        elements.clear();
+    }
 }
---
+++ data-structures/src/main/java/com/rocky/ao/queue/QueueStandardLibrary.java
@@ -0,0 +1,12 @@
+package com.rocky.ao.queue;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/21 20:02
+ * @description
+ */
+public class QueueStandardLibrary {
+    public static void main(String[] args) {
+
+    }
+}
