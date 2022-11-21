765a630d54f5c904c8a98d0cd8a64f5506db666a
Yun Ao <yun.ao@ninebot.com>
2022-11-21 16:14:37 +0800

    three way to deal with valid parentheses

--- algorithms/src/main/java/com/rocky/ao/stack/ValidParentheses.java
+++ algorithms/src/main/java/com/rocky/ao/stack/ValidParentheses.java
@@ -1,9 +1,81 @@
 package com.rocky.ao.stack;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 /**
  * @author yun.ao
  * @date 2022/11/21 15:48
  * @description
+ * https://leetcode.com/problems/valid-parentheses/
+ * https://leetcode.cn/problems/valid-parentheses/
  */
 public class ValidParentheses {
+    public boolean isValid1(String s) {
+        while (s.contains("{}") || s.contains("[]") || s.contains("()")) {
+            s = s.replace("{}", "");
+            s = s.replace("()", "");
+            s = s.replace("[]", "");
+        }
+
+        return s.isEmpty();
+    }
+
+    public boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char element = s.charAt(i);
+
+            if (element == '(' || element == '{' || element == '[') {
+                stack.push(element);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char left = stack.pop();
+                if (left == '(' && element != ')') {
+                    return false;
+                }
+
+                if (left == '[' && element != ']') {
+                    return false;
+                }
+
+                if (left == '{' && element != '}') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public boolean isValid3(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char element = s.charAt(i);
+
+            if (map.containsKey(element)) {
+                stack.push(element);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char left = stack.pop();
+
+                if (element != map.get(left)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
