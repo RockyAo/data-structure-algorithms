0870b096c89c34562930f1f87a8442c2ce8b4b70
Yun Ao <yun.ao@ninebot.com>
2022-11-22 16:33:33 +0800

    node construct functions

--- data-structures/src/main/java/com/rocky/ao/tree/Node.java
+++ data-structures/src/main/java/com/rocky/ao/tree/Node.java
@@ -10,4 +10,19 @@ public class Node<E> {
     Node<E> left;
     Node<E> right;
     Node<E> parent;
+
+    public Node(E element, Node<E> left, Node<E> right, Node<E> parent) {
+        this.element = element;
+        this.left = left;
+        this.right = right;
+        this.parent = parent;
+    }
+
+    public Node(E element, Node<E> parent) {
+        this(element, null, null, parent);
+    }
+
+    public Node(E element) {
+        this(element, null, null, null);
+    }
 }
