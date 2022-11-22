fb5e1c1af53888f962d7fdabd1e463f63bc454fd
Yun Ao <yun.ao@ninebot.com>
2022-11-22 16:25:51 +0800

    add binary search tree class

---
+++ data-structures/src/main/java/com/rocky/ao/tree/BinarySearchTree.java
@@ -0,0 +1,26 @@
+package com.rocky.ao.tree;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/22 16:24
+ * @description 二叉搜索树
+ */
+public class BinarySearchTree<E> {
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public void clear() {}
+
+    public void add(E element) {}
+
+    public void remove(E element) {}
+
+    public boolean contains(E element) {
+        return false;
+    }
+}
