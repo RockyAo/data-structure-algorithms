cf3cd02d03899a23329a8a5e27f3797f2f2d904f
Yun Ao <yun.ao@ninebot.com>
2022-11-24 15:34:46 +0800

    add Maximum Depth of Binary Tree class

---
+++ algorithms/src/main/java/com/rocky/ao/tree/MaximumDepthOfBinaryTree104.java
@@ -0,0 +1,11 @@
+package com.rocky.ao.tree;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/24 15:33
+ * @description
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+ */
+public class MaximumDepthOfBinaryTree104 {
+}
