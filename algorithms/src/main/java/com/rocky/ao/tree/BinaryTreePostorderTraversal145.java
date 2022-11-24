fba5a7d46983e4c687f553a32a0a663ef8100a40
Yun Ao <yun.ao@ninebot.com>
2022-11-24 12:01:54 +0800

    postorder traversal in recursion

--- algorithms/src/main/java/com/rocky/ao/tree/BinaryTreePostorderTraversal145.java
+++ algorithms/src/main/java/com/rocky/ao/tree/BinaryTreePostorderTraversal145.java
@@ -1,5 +1,8 @@
 package com.rocky.ao.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author yun.ao
  * @date 2022/11/24 11:54
@@ -8,5 +11,19 @@ package com.rocky.ao.tree;
  * https://leetcode.cn/problems/binary-tree-postorder-traversal/
  */
 public class BinaryTreePostorderTraversal145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        return postorderTraversalRecursion(root, new ArrayList<>());
+    }
 
+    private List<Integer> postorderTraversalRecursion(TreeNode root, List<Integer> res) {
+        if (root == null || res == null) {
+            return new ArrayList<>();
+        }
+
+        postorderTraversalRecursion(root.left, res);
+        postorderTraversalRecursion(root.right, res);
+
+        res.add(root.val);
+        return res;
+    }
 }
