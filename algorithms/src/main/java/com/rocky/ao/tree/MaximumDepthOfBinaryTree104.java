a55f924453bc96320e39c94feee368023d89a1e4
Yun Ao <yun.ao@ninebot.com>
2022-11-24 15:41:05 +0800

    104 max depth iteration

--- algorithms/src/main/java/com/rocky/ao/tree/MaximumDepthOfBinaryTree104.java
+++ algorithms/src/main/java/com/rocky/ao/tree/MaximumDepthOfBinaryTree104.java
@@ -1,5 +1,8 @@
 package com.rocky.ao.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author yun.ao
  * @date 2022/11/24 15:33
@@ -8,4 +11,34 @@ package com.rocky.ao.tree;
  * https://leetcode.cn/problems/maximum-depth-of-binary-tree/
  */
 public class MaximumDepthOfBinaryTree104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) { return 0;}
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int levelSize = 1;
+        int height = 0;
+
+        while (!queue.isEmpty()) {
+            levelSize --;
+
+            TreeNode node = queue.poll();
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+
+            if (levelSize == 0) {
+                levelSize = queue.size();
+                height += 1;
+            }
+        }
+
+        return height;
+    }
 }
