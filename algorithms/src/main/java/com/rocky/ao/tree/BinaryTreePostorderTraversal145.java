99146f31f39ee06ac3cd92b0d779e122452b530e
Yun Ao <yun.ao@ninebot.com>
2022-11-24 14:37:12 +0800

    postorder traversal iteration way

--- algorithms/src/main/java/com/rocky/ao/tree/BinaryTreePostorderTraversal145.java
+++ algorithms/src/main/java/com/rocky/ao/tree/BinaryTreePostorderTraversal145.java
@@ -1,6 +1,8 @@
 package com.rocky.ao.tree;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -12,7 +14,33 @@ import java.util.List;
  */
 public class BinaryTreePostorderTraversal145 {
     public List<Integer> postorderTraversal(TreeNode root) {
-        return postorderTraversalRecursion(root, new ArrayList<>());
+//        return postorderTraversalRecursion(root, new ArrayList<>());
+
+        List<Integer> res = new ArrayList<>();
+
+        Deque<TreeNode> stack = new LinkedList<>();
+
+        TreeNode prev = null;
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            if (root.right == null || root.right == prev) {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+
+        return res;
     }
 
     private List<Integer> postorderTraversalRecursion(TreeNode root, List<Integer> res) {
