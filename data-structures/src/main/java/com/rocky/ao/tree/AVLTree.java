2135e3b052474b4875add19823c6423bcd1444a1
Yun Ao <yun.ao@ninebot.com>
2022-11-24 17:07:52 +0800

    AVL constructor functions

--- data-structures/src/main/java/com/rocky/ao/tree/AVLTree.java
+++ data-structures/src/main/java/com/rocky/ao/tree/AVLTree.java
@@ -1,9 +1,18 @@
 package com.rocky.ao.tree;
 
+import java.util.Comparator;
+
 /**
  * @author yun.ao
  * @date 2022/11/24 16:55
  * @description
  */
-public class AVLTree<E> extends BinaryTree<E> {
+public class AVLTree<E> extends BinarySearchTree<E> {
+    AVLTree() {
+        this(null);
+    }
+
+    AVLTree(Comparator<E> comparator) {
+        super(comparator);
+    }
 }
