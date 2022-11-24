aee812459ef8d2fa8793fc6fd52b38886f9d0a81
Yun Ao <yun.ao@ninebot.com>
2022-11-24 16:30:26 +0800

    DFS

--- algorithms/src/main/java/com/rocky/ao/tree/_662_MaximumWidthOfBinaryTree.java
+++ algorithms/src/main/java/com/rocky/ao/tree/_662_MaximumWidthOfBinaryTree.java
@@ -1,9 +1,29 @@
 package com.rocky.ao.tree;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  * @author yun.ao
  * @date 2022/11/24 16:11
  * @description
+ * https://leetcode.com/problems/maximum-width-of-binary-tree/
+ * https://leetcode.cn/problems/maximum-width-of-binary-tree/
  */
 public class _662_MaximumWidthOfBinaryTree {
+    Map<Integer, Integer> levelMin = new HashMap<Integer, Integer>();
+
+    public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 1, 1);
+    }
+
+    public int dfs(TreeNode node, int depth, int index) {
+        if (node == null) {
+            return 0;
+        }
+        levelMin.putIfAbsent(depth, index); // 每一层最先访问到的节点会是最左边的节点，即每一层编号的最小值
+        return Math.max(index - levelMin.get(depth) + 1, Math.max(dfs(node.left, depth + 1, index * 2), dfs(node.right, depth + 1, index * 2 + 1)));
+    }
 }
