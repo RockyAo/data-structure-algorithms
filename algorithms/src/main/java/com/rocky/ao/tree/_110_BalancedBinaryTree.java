23269d3223cefda763408f0fd4cc5660a3ed4c0d
Yun Ao <yun.ao@ninebot.com>
2022-11-25 18:48:48 +0800

    add balance binary tree class

---
+++ algorithms/src/main/java/com/rocky/ao/tree/_110_BalancedBinaryTree.java
@@ -0,0 +1,14 @@
+package com.rocky.ao.tree;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/25 18:48
+ * @description
+ * https://leetcode.com/problems/balanced-binary-tree/
+ * https://leetcode.cn/problems/balanced-binary-tree/
+ */
+public class _110_BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+
+    }
+}
