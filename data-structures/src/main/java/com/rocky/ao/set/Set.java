d64e7311213edb108b243f19f461c6a85ecb05b4
Yun Ao <yun.ao@ninebot.com>
2022-11-29 16:58:22 +0800

    add set interface and classes

---
+++ data-structures/src/main/java/com/rocky/ao/set/Set.java
@@ -0,0 +1,11 @@
+package com.rocky.ao.set;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/29 16:57
+ * @description
+ */
+public interface Set<E> {
+    int size();
+    boolean isEmpty();
+}
---
+++ data-structures/src/main/java/com/rocky/ao/set/TreeSet.java
@@ -0,0 +1,9 @@
+package com.rocky.ao.set;
+
+/**
+ * @author yun.ao
+ * @date 2022/11/29 16:58
+ * @description
+ */
+public class TreeSet {
+}
