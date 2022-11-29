ce9ef4deab3d615b83dd5ca5bc2dc2cd15c777b6
Yun Ao <yun.ao@ninebot.com>
2022-11-29 17:00:10 +0800

    finished set interface functions

--- data-structures/src/main/java/com/rocky/ao/set/Set.java
+++ data-structures/src/main/java/com/rocky/ao/set/Set.java
@@ -8,4 +8,14 @@ package com.rocky.ao.set;
 public interface Set<E> {
     int size();
     boolean isEmpty();
+    void clear();
+    boolean contains(E element);
+    void add(E element);
+    void remove(E element);
+    void traversal(Visitor<E> visitor);
+
+    public static abstract class Visitor<E> {
+        boolean isStop;
+        abstract boolean visit(E element);
+    }
 }
