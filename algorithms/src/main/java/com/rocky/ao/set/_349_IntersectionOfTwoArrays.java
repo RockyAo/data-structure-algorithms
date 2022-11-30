67f5ba08cf8bd7b9be7e82dfdfb3e930b14ddb33
Yun Ao <yun.ao@ninebot.com>
2022-11-30 10:33:24 +0800

    unimplement

--- algorithms/src/main/java/com/rocky/ao/set/_349_IntersectionOfTwoArrays.java
+++ algorithms/src/main/java/com/rocky/ao/set/_349_IntersectionOfTwoArrays.java
@@ -1,5 +1,12 @@
 package com.rocky.ao.set;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * @author yun.ao
  * @date 2022/11/29 17:19
@@ -8,4 +15,7 @@ package com.rocky.ao.set;
  * https://leetcode.cn/problems/intersection-of-two-arrays/
  */
 public class _349_IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        return null;
+    }
 }
