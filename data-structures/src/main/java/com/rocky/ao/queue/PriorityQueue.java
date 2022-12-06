378c59bf341e07c1ef20ccd7ce0c245f0bc8c389
Yun Ao <yun.ao@ninebot.com>
2022-12-06 15:42:47 +0800

    priority queue

--- data-structures/src/main/java/com/rocky/ao/queue/PriorityQueue.java
+++ data-structures/src/main/java/com/rocky/ao/queue/PriorityQueue.java
@@ -1,36 +1,48 @@
 package com.rocky.ao.queue;
 
+import com.rocky.ao.heap.BinaryHeap;
 import com.rocky.ao.queue.Queue;
+
+import java.util.Comparator;
 
 /**
  * @author yun.ao
  * @date 2022/12/6 11:40
- * @description
+ * @description 优先级队列
  */
 public class PriorityQueue<E> {
+    // 也可直接用数组实现（原理还是用数组实现二叉堆）
+    private BinaryHeap<E> heap;
 
+    public PriorityQueue(Comparator<E> comparator) {
+        heap = new BinaryHeap<>(comparator);
+    }
+
+    public PriorityQueue() {
+        this(null);
+    }
 
     public int size() {
-        return elements.size();
+        return heap.size();
     }
 
     public boolean isEmpty() {
-        return elements.isEmpty();
-    }
-
-    public void enqueue(E element) {
-        elements.add(element);
-    }
-
-    public E dequeue() {
-        return elements.removeFirst();
-    }
-
-    public E peek() {
-        return elements.getFirst();
+        return heap.isEmpty();
     }
 
     public void clear() {
-        elements.clear();
+        heap.clear();
+    }
+
+    public void enQueue(E element) {
+        heap.add(element);
+    }
+
+    public E deQueue() {
+        return heap.remove();
+    }
+
+    public E front() {
+        return heap.get();
     }
 }
