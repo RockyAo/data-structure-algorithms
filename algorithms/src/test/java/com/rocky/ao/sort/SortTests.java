8f241c3d286a6d5f3993d3cb7edb5a263a61b740
Yun Ao <yun.ao@ninebot.com>
2022-12-08 22:43:47 +0800

    bubble sort easy way

--- algorithms/src/test/java/com/rocky/ao/sort/SortTests.java
+++ algorithms/src/test/java/com/rocky/ao/sort/SortTests.java
@@ -1,9 +1,43 @@
 package com.rocky.ao.sort;
 
+
+import com.rocky.utils.Integers;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * @author yun.ao
  * @date 2022/12/8 17:05
  * @description
  */
+
 public class SortTests {
+
+    @Test
+    public void testAllSorts() {
+        Integer[] randomData = Integers.random(20, 0, 20);
+
+        testSorts(
+                randomData,
+                new BubbleSort1<>()
+                );
+    }
+
+    private void testSorts(Integer[] array, Sort<Integer>... sorts) {
+        for (Sort<Integer> sort : sorts) {
+            Integer[] newArray = Integers.copy(array);
+            Integers.println(newArray);
+            sort.sort(newArray);
+            Integers.println(newArray);
+            Assert.assertTrue(Integers.isAscOrder(newArray));
+        }
+
+        Arrays.sort(sorts);
+
+        for (Sort<Integer> sort : sorts) {
+            System.out.println(sort);
+        }
+    }
 }
