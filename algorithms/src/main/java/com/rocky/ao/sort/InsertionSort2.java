808707e908d70aae3eba81b4e6ae48bc9376f08f
Yun Ao <yun.ao@ninebot.com>
2022-12-09 15:45:49 +0800

    Insertion Sort 2 class

---
+++ algorithms/src/main/java/com/rocky/ao/sort/InsertionSort2.java
@@ -0,0 +1,21 @@
+package com.rocky.ao.sort;
+
+/**
+ * @author yun.ao
+ * @date 2022/12/7 10:55
+ * @description
+ */
+public class InsertionSort2<T extends Comparable<T>> extends Sort<T> {
+
+    @Override
+    protected void sort() {
+        for (int start = 1; start < data.length; start++) {
+            int current = start;
+
+            while (current > 0 && cmp(current, current-1) < 0) {
+                swap(current, current-1);
+                current--;
+            }
+        }
+    }
+}
