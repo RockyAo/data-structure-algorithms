991dbd37161789081322a61537206966eec9233e
Yun Ao <yun.ao@ninebot.com>
2022-12-09 15:32:31 +0800

    insertion sort class

---
+++ algorithms/src/main/java/com/rocky/ao/sort/InsertionSort.java
@@ -0,0 +1,14 @@
+package com.rocky.ao.sort;
+
+/**
+ * @author yun.ao
+ * @date 2022/12/7 10:55
+ * @description
+ */
+public class InsertionSort<T extends Comparable<T>> extends Sort<T> {
+
+    @Override
+    protected void sort() {
+
+    }
+}
