7bca3f2407486fe170e11a5f57b5df59471d988f
Yun Ao <yun.ao@ninebot.com>
2023-01-31 21:34:09 +0800

    add quick sort class

---
+++ algorithms/src/main/java/com/rocky/ao/sort/QuickSort.java
@@ -0,0 +1,14 @@
+package com.rocky.ao.sort;
+
+/**
+ * @author yun.ao
+ * @date 2023/1/31 21:33
+ * @description
+ */
+public class QuickSort<T extends Comparable<T>> extends Sort<T> {
+
+    @Override
+    protected void sort() {
+        
+    }
+}
